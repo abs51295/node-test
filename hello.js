8fb9658b06fba7e113c8477754ec5b886b413fd4
Aagam Shah <aagam@redhat.com>
2018-01-15 01:56:16 +0530

    Upload file to s3 after saving

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
 const registry = require('package-stream')()
 var jsonfile = require('jsonfile')
+var fs = require('fs'),
+    S3FS = require('s3fs'),
+    s3fsImpl = new S3FS(process.env.AWS_BUCKET_NAME, {
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+        secretAccessKey:  process.env.AWS_S3_SECRET_KEY,
+        region: "us-east-2",
+        signatureVersion: "v4",
+        s3DisableBodySigning: false
+    });
 var file = 'node-package-details.json'
-var count = 0
+var count = 0;
+var once = false;
 registry
     .on('package', function (pkg) {
         console.log(count++)
@@ -16,7 +26,19 @@ registry
     })
     .on('up-to-date', function () {
         console.log('Done with collecting data')
-        process.stdout.write(JSON.stringify(dependents))
-        process.exit()
-    })
+        //process.stdout.write(JSON.stringify(dependents))
+        if(!once) {
+            var stream = fs.createReadStream(file);
+            s3fsImpl.writeFile(file, stream).then(function () {
+                fs.unlink(file, function (err) {
+                    if (err) {
+                        console.error(err);
+                        process.exit();
+                    }
+                });
+                process.exit();
+            });
+            once  = true;
+        }
+    });
 
